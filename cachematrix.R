40f99815a9767e7c1d7fd2414a9b79e47afb7f39
Nathan Loyer <nathan.loyer@gmail.com>
2015-04-25 03:00:16 -0400

    my changes to cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Usage:
+## x = makeCacheMatrix(matrix(sample(1000000),1000))
+##
+## inv = cacheSolve(x)
+##    - actually calculated
+## inv = cacheSolve(x)
+##    - retreived from cache
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## description:
+##    this function returns a cacheMatrix object with the methods
+##    get() and cacheSolve()
+## input:
+##    matrix object
+## output:
+##    cacheMatrix Object
+makeCacheMatrix <- function(cachedMatrix = matrix()) {
+  
+  cachedInv <- NULL
+  
+  ## return the matrix
+  get <- function() {
+    cachedMatrix
+  }
+  
+  ## tests to see if we have an inverted matrix cached, then returns it
+  ## or computes the inverted matrix then returns that
+  cacheSolve <- function() {
+    if(!is.null(cachedInv)) {
+      message("getting cached data")
+      return(cachedInv)
+    }    
+    cachedInv <<- solve(cachedMatrix)
+    cachedInv
+  }
+  
+  # make our object it's own class with methods and return it
+  self = list(get = get, cacheSolve = cacheSolve)
+  class(self) <- c('cacheMatrix', 'list', 'matrix')
+  self
 }
 
+## wrapper around the nested cacheSolve method, just to satisy
+## the parameters of the assignment
+cacheSolve <- function(x) {
+  ## Return a matrix that is the inverse of 'x'
+  x$cacheSolve()
+}
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## this overloads the default print for our object, cacheMatrix, 
+## so it looks like a normal matrix when you print it
+print.cacheMatrix <- function(x) {
+  print(x$get())
 }
